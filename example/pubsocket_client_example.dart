1eb56edfaa8b4fc4a961adf7b6ad2c0908b6ccf1
Vinicius <antoniovinicius1801@gmail.com>
2021-10-01 10:10:55 -0300

    first commit

---
+++ example/pubsocket_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:pubsocket_client/pubsocket_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/pubsocket_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pubsocket_client;
+
+export 'src/pubsocket_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
